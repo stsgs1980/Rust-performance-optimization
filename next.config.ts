a98d84deda7b76b07887e2269b097e9e474be3d8
Stsgs1980 <stsgs1980@gmail.com>
2026-06-30 16:04:30 +0300

    fix: add turbopack root to fix Vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   output: "standalone",
@@ -7,6 +8,9 @@ const nextConfig: NextConfig = {
   },
   reactStrictMode: false,
   allowedDevOrigins: ["http://127.0.0.1:3000", "http://localhost:3000", "http://21.0.17.239:3000"],
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
